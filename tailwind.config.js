bf3ca33e2f70e4bd526edf7220dffab90b7d73b7
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-08-15 12:34:17 -0400

    update tailwind add plugins & delete custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,6 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
@@ -33,6 +22,9 @@ module.exports = {
       }
     }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
 }
 
