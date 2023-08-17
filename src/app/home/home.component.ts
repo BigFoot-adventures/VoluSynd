7f20cf352c92457ef17b48a4808cb47083463c03
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-08-17 14:26:43 -0400

    added loggedIn variable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  loggedIn = false;
+
+  constructor(private userSvc: UserService) {
+    this.loggedIn = userSvc.userToken ? true : false;
+    console.log(this.loggedIn);
+  }
 
 }
